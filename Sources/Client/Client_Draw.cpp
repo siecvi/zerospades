b448856406b92a8768479d7ada8d17f337142302
siecvi <enzo_tw_14@hotmail.com>
2022-04-02 16:48:09 -0300

    Display stats even when hud is disabled
    Turns out this is intended, could be useful and can be easily disabled in the settings.

--- Sources/Client/Client_Draw.cpp
+++ Sources/Client/Client_Draw.cpp
@@ -937,7 +937,7 @@ namespace spades {
 			if (IsLimboViewActive())
 				limbo->Draw();
 
-			if (shouldDraw && cg_stats)
+			if (cg_stats)
 				DrawStats(sw, sh);
 		}
 
@@ -1022,8 +1022,8 @@ namespace spades {
 
 			if (net) {
 				auto ping = net->GetPing();
-				auto upbps = (int)net->GetUplinkBps() / 1000;
-				auto downbps = (int)net->GetDownlinkBps() / 1000;
+				auto upbps = (int)(net->GetUplinkBps() / 1000);
+				auto downbps = (int)(net->GetDownlinkBps() / 1000);
 				sprintf(buf, "ping: %dms, up/down: %d/%dkbps", ping, upbps, downbps);
 				str += buf;
 			}
