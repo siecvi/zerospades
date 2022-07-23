57e48a2e83df20c7378993cdd0038f91f70c197d
David CARLIER <devnexen@gmail.com>
2022-07-23 17:42:14 +0100

    Adding likely/unlikely to optimise some code paths.

--- Sources/Audio/ALDevice.cpp
+++ Sources/Audio/ALDevice.cpp
@@ -443,7 +443,7 @@ namespace spades {
 					dev = NULL;
 				alDevice = al::qalcOpenDevice(dev);
 
-				if (!alDevice) {
+				if (UNLIKELY(!alDevice)) {
 					if ((ext = al::qalcGetString(NULL, ALC_EXTENSIONS))) {
 						std::vector<std::string> strs = Split(ext, " ");
 						SPLog("OpenAL ALC Extensions (NULL):");
@@ -466,7 +466,7 @@ namespace spades {
 				}
 
 				alContext = al::qalcCreateContext(alDevice, NULL);
-				if (!alContext) {
+				if (UNLIKELY(!alContext)) {
 					al::qalcCloseDevice(alDevice);
 					SPRaise("Failed to create OpenAL context.");
 				}
@@ -564,7 +564,7 @@ namespace spades {
 				SPADES_MARK_FUNCTION();
 
 				ALSrc* src = AllocChunk();
-				if (!src)
+				if (UNLIKELY(!src))
 					return;
 
 				src->stereo = chunk->GetFormat() == AL_FORMAT_STEREO16;
